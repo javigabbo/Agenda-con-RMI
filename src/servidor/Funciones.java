0750793e1f1abefcd938856a4aebe791f5b1e737
javigabbo <javigabbo@gmail.com>
2017-01-13 10:20:19 +0100

    Commit con la interfaz creada.

---
+++ src/servidor/Funciones.java
@@ -0,0 +1,13 @@
+package servidor;
+
+public interface Funciones {
+
+	public static void muestraContactos(){}
+	
+	public static void nuevoContacto(){}
+	
+	public static void modificaContacto(){}
+	
+	public static void eliminaContacto(){}
+	
+}
